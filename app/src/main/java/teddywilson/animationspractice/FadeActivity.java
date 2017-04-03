55f75746a5c6eeeda545f76bfe764fadf16a70e0
Teddy Wilson <teddy@toeboat.io>
2017-04-03 15:04:56 -0400

    finished fade

--- app/src/main/java/teddywilson/animationspractice/FadeActivity.java
+++ app/src/main/java/teddywilson/animationspractice/FadeActivity.java
@@ -2,6 +2,9 @@ package teddywilson.animationspractice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 /**
  * Created by Theodore Wilson on 4/3/17.
@@ -10,10 +13,65 @@ import android.os.Bundle;
 
 public class FadeActivity extends Activity{
 
+    View fadeView;
+    boolean isFaded = false;
+    int duration = 1500;
+
+    AlphaAnimation fadeOut, fadeIn;
+
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
+        setContentView(R.layout.activity_fade);
+        fadeView = findViewById(R.id.view_to_fade);
+
+        fadeOut = new AlphaAnimation(1.0f, 0.f);
+        fadeOut.setDuration(duration);
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                fadeView.setAlpha(0.0f);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        fadeIn = new AlphaAnimation(0.0f, 1.0f);
+        fadeIn.setDuration(duration);
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                fadeView.setAlpha(1.0f);
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
 
     }
+
+    public void toggleFade(View v){
+        if(isFaded){
+            fadeView.startAnimation(fadeIn);
+        } else {
+            fadeView.startAnimation(fadeOut);
+        }
+        isFaded = !isFaded;
+    }
+
 
 }
